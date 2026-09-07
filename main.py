c93547abb17b5337376ff2aa3900fc0daeadf4a4
Sumit Maurya <mauryasumit222@gmail.com>
2026-09-07 15:48:40 +0530

    feat: implement TaskModel and register it in the application database initialization

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from src.utils.db import Base, engine
+from src.tasks.models import TaskModel
 
 Base.metadata.create_all(engine)
 
---
+++ src/tasks/models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, DateTime
+from src.utils.db import Base
+
+
+class TaskModel(Base):
+    __tablename__ = "user_tasks"
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    description = Column(String)
+    is_completed = Column(Boolean, default=False)
